e8feaa97e6c17d9eb1e567f143c529864a103d31
sougabioliveira <oliveira.gabrielagomes@gmail.com>
2022-08-23 23:31:50 -0300

    Preparação inicial.

---
+++ src/test/java/com/urna/urnaeletronica/UrnaEletronicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urna.urnaeletronica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrnaEletronicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
