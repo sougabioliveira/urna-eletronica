5f57901346d841e237ab0bb39c172666a4123e58
sougabioliveira <oliveira.gabrielagomes@gmail.com>
2022-08-25 01:22:50 -0300

    Adicionado ID, nome do vice, legenda e data registro

--- src/main/java/com/urna/urnaeletronica/model/Candidato.java
+++ src/main/java/com/urna/urnaeletronica/model/Candidato.java
@@ -3,11 +3,30 @@ package com.urna.urnaeletronica.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
 @Data
+@Entity
+@Table(name = "candidato")
 public class Candidato {
 
-    @JsonProperty("nome")
-    private String nome;
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @JsonProperty("NomeCompleto")
+    private String nomeCompleto;
+
+    @JsonProperty("NomeVice")
+    private String nomeVice;
+
+    @JsonProperty("Legenda")
+    private Integer legenda;
+
+    @JsonProperty("DataRegistro")
+    private LocalDateTime dataRegistro;
 }
 
 
