c65baca6fe5ec58739842c8373c06dc49f0cd264
sougabioliveira <oliveira.gabrielagomes@gmail.com>
2022-08-25 01:21:43 -0300

    Adicionado mais informações ao candidato

--- src/main/java/com/urna/urnaeletronica/controller/CandidatoController.java
+++ src/main/java/com/urna/urnaeletronica/controller/CandidatoController.java
@@ -2,19 +2,25 @@ package com.urna.urnaeletronica.controller;
 
 
 import com.urna.urnaeletronica.model.Candidato;
+import com.urna.urnaeletronica.repository.CandidatoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/candidatos")
 public class CandidatoController {
 
+    @Autowired
+    private CandidatoRepository candidatoRepository;
+
     @GetMapping
-    public ResponseEntity<Candidato> getAll() {
-        Candidato candidato = new Candidato();
-        candidato.setNome("Candidato 1");
-        return ResponseEntity.ok(candidato);
+    public ResponseEntity<List<Candidato>> getAll() {
+        List<Candidato> candidatos = candidatoRepository.findAll();
+        return ResponseEntity.ok(candidatos);
     }
 }
